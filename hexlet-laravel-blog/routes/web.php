53b4b66bbfdfa8ed82c1402e7863e3da93510936
Airat29 <fat.airat@rambler.ru>
2025-01-21 19:25:25 +0300

    We can read a WATCH a CRUD

--- hexlet-laravel-blog/routes/web.php
+++ hexlet-laravel-blog/routes/web.php
@@ -18,5 +18,9 @@ Route::get('/', function () {
 });
 
 Route::get('about', [PageController::class, 'about']);
+
 Route::get('articles', [ArticleController::class, 'index'])
   ->name('articles.index');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+  ->name('articles.show');
